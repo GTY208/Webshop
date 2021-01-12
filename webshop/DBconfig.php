ae3a5ba8fbf0e0500347795a6ef4bac0c610ce4a
Gideon <2101121@talnet.nl>
2021-01-12 17:53:20 +0100

    NEW LESSON PHP

--- webshop/DBconfig.php
+++ webshop/DBconfig.php
@@ -1,14 +1,15 @@
 <?php
-DEFINE("USER", "root");
-DEFINE("PASSWORD", "root");
-try{
-    $verbinding = new
-    PDO("mysql:host=localhost;dbname=webshop", USER,PASSWORD);
-    $verbinding -> collator_set_attribute
-(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-}catch(PDOExeption $e) {
-    echo $e -> getMessage();
-    echo "Kon geen verbinding maken.";
 
+DEFINE("USER", "root");
+DEFINE("PASSWORD", "");
+
+try{
+    $verbinding = new 
+    PDO("mysql:host=localhost;dbname=webshop", USER, PASSWORD);
+    $verbinding->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOExeption $e){
+    echo $e->getMessage();
+    echo "Kon geen verbinding maken";
 }
+
 ?>
--- webshop/index.php
+++ webshop/index.php
@@ -8,7 +8,7 @@ if(isset($_GET["page"])) {
     $page = "inloggen";
 }
 if($page) {
-    include("page/" . $page . ".php");
+    include("pages/" . $page . ".php");
 }
 
 ?>
